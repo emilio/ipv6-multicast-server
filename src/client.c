72a678e50d2c58491341f73010acacb39bbdaeed
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-05-04 14:31:35 +0200

    client: Bootstrap client

---
+++ src/client.c
@@ -0,0 +1,133 @@
+/**
+ * client.c:
+ *   Main client logic
+ *
+ * Copyright (C) 2015 Emilio Cobos Álvarez (70912324N) <emiliocobos@usal.es>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * server.c:
+ *   UDP server's logic
+ *
+ * Copyright (C) 2015 Emilio Cobos Álvarez (70912324N) <emiliocobos@usal.es>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <assert.h>
+
+#include "logger.h"
+#include "config.h"
+#include "socket-utils.h"
+
+/// Shows usage of the program
+void show_usage(int _argc, char** argv) {
+    fprintf(stderr, "Usage: %s [options]\n", argv[0]);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  -h, --help\t Display this message and exit\n");
+    fprintf(stderr, "  -a, --address [address]\t IPv6 address\n");
+    fprintf(stderr, "  -i, --interface [iface]\t network interface\n");
+    fprintf(stderr, "  -p, --port [port]\t Listen to [port]\n");
+    fprintf(stderr, "  -v, --verbose\t Be verbose about what is going on\n");
+    fprintf(stderr, "  -l, --log [file]\t Log to [file]\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Author(s):\n");
+    fprintf(stderr, "  Emilio Cobos Álvarez (<emiliocobos@usal.es>)\n");
+}
+
+
+int main(int argc, char** argv) {
+    const char* ip_address = "ff02:0:0:0:0:0:0:f";
+    const char* interface = NULL;
+    const char* port = "8000";
+
+    LOGGER_CONFIG.log_file = stderr;
+
+    for(int i  = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            show_usage(argc, argv);
+            return 1;
+        } else if (strcmp(argv[i], "-v") == 0 ||
+                   strcmp(argv[i], "--verbose") == 0) {
+            LOGGER_CONFIG.verbose = true;
+        } else if (strcmp(argv[i], "-l") == 0 ||
+                   strcmp(argv[i], "--log") == 0) {
+            ++i;
+            if (i == argc)
+                FATAL("The %s option needs a value", argv[i - 1]);
+
+            FILE* log_file = fopen(argv[i], "w");
+            if (log_file)
+                LOGGER_CONFIG.log_file = log_file;
+            else
+                WARN("Could not open \"%s\", using stderr: %s", argv[i],
+                     strerror(errno));
+        } else if (strcmp(argv[i], "-p") == 0 ||
+                   strcmp(argv[i], "--port") == 0) {
+            ++i;
+            if (i == argc)
+                FATAL("The %s option needs a value", argv[i - 1]);
+            port = argv[i];
+        } else if (strcmp(argv[i], "-a") == 0 ||
+                   strcmp(argv[i], "--address") == 0) {
+            ++i;
+            if (i == argc)
+                FATAL("The %s option needs a value", argv[i - 1]);
+            ip_address = argv[i];
+        } else if (strcmp(argv[i], "-i") == 0 ||
+                   strcmp(argv[i], "--interface") == 0) {
+            ++i;
+            if (i == argc)
+                FATAL("The %s option needs a value", argv[i - 1]);
+            interface = argv[i];
+        } else {
+            WARN("Unhandled option: %s", argv[i]);
+        }
+    }
+
+    LOG("Using iface: %s, port: %s, address: %s", interface, port, ip_address);
+
+
+    if (LOGGER_CONFIG.log_file)
+        fclose(LOGGER_CONFIG.log_file);
+
+    return 0;
+}
